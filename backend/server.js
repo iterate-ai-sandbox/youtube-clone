5e0926f4828688ee057a862b27d206cf195d5f2d
ch1nru5t <chinrust@gmail.com>
2024-07-20 21:17:20 +0530

    remove localhost

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://iterate.rkph.me",
     // allowedHeaders: [
     //   "Content-Type",
     //   "Authorization",
